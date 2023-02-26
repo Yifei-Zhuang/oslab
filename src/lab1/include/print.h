5ecbd0a988382aba3451e90b4a40a3b198d664c3
IDEA <3200105872@zju.edu.cn>
2023-02-26 23:00:57 +0800

    docs:remove redundant file

--- src/lab1/include/print.h
+++
@@ -1,7 +0,0 @@
-#ifndef _PRINT_H
-#define _PRINT_H
-#include "types.h"
-void puts(char *s);
-void puti(uint64 x);
-
-#endif
--- src/lab3/include/clock.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include"sbi.h"
-unsigned long get_cycles();
-void clock_set_next_event();
-
